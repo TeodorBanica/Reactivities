43bac0fbc54de01bf79f07f89914266005908669
Teodor Banica <teodor.banica@totalsoft.ro>
2020-10-04 21:59:20 +0300

    end section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -12,6 +12,8 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Persistance;
 using Microsoft.EntityFrameworkCore;
+using MediatR;
+using Application.Activities;
 
 
 namespace API
@@ -29,16 +31,20 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
             services.AddDbContext<DataContext>(opt =>
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+
             services.AddCors(opt =>{
                 opt.AddPolicy("CorsPolicy",policy =>
                 {
                     policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
                 });
             });
+
+            services.AddMediatR(typeof(ListActivities.Handler).Assembly);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
