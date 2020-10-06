253f6cc8a1391c5f6ca5f466258e693ca5a2d92a
Teodor Banica <teodor.banica@totalsoft.ro>
2020-10-06 13:21:11 +0300

    end section 6

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -9,10 +9,11 @@ interface IProps{
     activity: IActivity;
     createActivity: (activity: IActivity) => void;
     editActivity: (activity: IActivity) => void;
+    submitting: boolean;
 
 }
 
-const ActivityForm: React.FC<IProps> = ({setEditMode, activity: initialFormState, createActivity, editActivity }) => {
+const ActivityForm: React.FC<IProps> = ({setEditMode, activity: initialFormState, createActivity, editActivity, submitting }) => {
 
     const InitializeForm = () =>{
         if(initialFormState){
@@ -61,7 +62,7 @@ const ActivityForm: React.FC<IProps> = ({setEditMode, activity: initialFormState
                 <Form.Input onChange={handleInputChange} name='date' type = 'datetime-local' placeholder = 'Date' value = {activity.date}/>
                 <Form.Input onChange={handleInputChange} name='city' placeholder = 'City' value = {activity.city}/>
                 <Form.Input onChange={handleInputChange} name='venue' placeholder = 'Venue' value = {activity.venue}/>
-                <Button floated='right' positive type='submit' content = 'Submit'/>
+                <Button loading={submitting} floated='right' positive type='submit' content = 'Submit'/>
                 <Button floated='right' type='button' content = 'Cancel'/>
 
             </Form>
