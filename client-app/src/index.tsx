fe513f1c263c0fa2e394b48367b184f9823e03c9
Teodor Banica <teodor.banica@totalsoft.ro>
2020-10-09 13:23:34 +0300

    d

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -7,6 +7,10 @@ import './app/layout/styles.css';
 import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
 import ScrollToTop from './app/layout/ScrollToTop';
+import 'react-widgets/dist/css/react-widgets.css';
+import dateFnsLocalizer from 'react-widgets-date-fns';
+
+new dateFnsLocalizer();
 
 export const history = createBrowserHistory();
 
