fcd9e3b9b7daf8cce3f0506d5ada9000598773d1
Teodor Banica <teodor.banica@totalsoft.ro>
2020-10-15 16:41:56 +0300

    end section 20

--- client-app/src/features/profiles/ProfilePage.tsx
+++ client-app/src/features/profiles/ProfilePage.tsx
@@ -16,7 +16,7 @@ interface IProps extends RouteComponentProps<RouteParams> {}
 const ProfilePage: React.FC<IProps> = ({match}) => {
 
     const rootStore = useContext(RootStoreContext);
-    const {loadingProfile, profile, loadProfile} = rootStore.profileStore;
+    const {loadingProfile, profile, loadProfile, follow, unfollow, isCurrentUser, loading, setActiveTab} = rootStore.profileStore;
 
     useEffect(() => {
         loadProfile(match.params.username)
@@ -26,12 +26,19 @@ const ProfilePage: React.FC<IProps> = ({match}) => {
 
     return (
         <Grid>
-            <Grid.Column width = {16}>
-                <ProfileHeader profile = {profile!} />
-                <ProfileContent/>
-            </Grid.Column>
+          <Grid.Column width={16}>
+            <ProfileHeader
+              profile={profile!}
+              isCurrentUser={isCurrentUser}
+              loading={loading}
+              follow={follow}
+              unfollow={unfollow}
+            />
+            <ProfileContent setActiveTab={setActiveTab} />
+          </Grid.Column>
         </Grid>
-    )
-}
+      );
+    };
+    
 
 export default observer(ProfilePage)
